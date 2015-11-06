7223be284c1a9d5a6f2e7a859cb15be6e67160c4
kawine <kawin.ethayarajh@gmail.com>
2015-11-05 23:07:29 -0500

    replaced javascript hack for back button with legitimate approach

--- js/init.js
+++ js/init.js
@@ -2,7 +2,27 @@
 
 $(".parallax").parallax();
 $(".toc-content").children().hide();
-$("#letter-from-the-editor").show();
+
+var pathArray = window.location.href.split( '/' );
+
+if ((pathArray.length > 3) && (pathArray[3] !== "") && (pathArray[3].indexOf("#") == 0)) {
+	console.log(pathArray[3]);
+	console.log(pathArray.length);
+	window.location.href = "#table-of-contents";
+
+	if (pathArray[3] == "#table-of-contents") {
+		pathArray[3] = "#letter-from-the-editor";
+	} 
+
+	$(pathArray[3]).show();
+	$('a[href=' + pathArray[3] + ']').addClass("chapter-active");
+	$('a[href=' + pathArray[3] + ']').addClass("disabled");
+	
+} else {
+	$("#letter-from-the-editor").show();
+	$('a[href=' + "#letter-from-the-editor" + ']').addClass("chapter-active");
+        $('a[href=' + "#letter-from-the-editor" + ']').addClass("disabled");
+} 
 
 $(function(){
 
